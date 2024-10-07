429ade3128594776dcc044e701d91b8faab08724
alif.meydi <alif.meydi@rds.co.id>
2024-10-07 11:21:33 +0700

    feat: first commit

---
+++ GenericClassTypeInMethod(eps-3)/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericClassTypeInMethod_eps_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> boxStr = new Box<string>("Test");
+            boxStr.UpdateContent("updatenya nih");
+            Console.WriteLine(boxStr.GetContent());
+
+            Box<int> boxInt = new Box<int>(123);
+            boxInt.UpdateContent(30);
+            Console.WriteLine(boxInt.GetContent());
+        }
+    }
+}
